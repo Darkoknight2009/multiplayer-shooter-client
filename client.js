f0b9720a0cea97d3bd08ccee10ca4273bb820445
Darkoknight2009 <147906617+Darkoknight2009@users.noreply.github.com>
2024-10-01 11:16:34 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('https://your-glitch-project-url.glitch.me'); // Link to the Glitch server
+const socket = io('https://butter-battle-purpose.glitch.me/'); // Link to the Glitch server
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
